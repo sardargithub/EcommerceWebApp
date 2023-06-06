a7ff627d59d1d6d589bca8b224afd01f2dd9ea4a
sardargithub <shaiksardarhussain1996@gmail.com>
2023-06-06 18:35:50 +0530

    first commit

---
+++ src/main/java/EcommerceMain/EcomWeb/App.java
@@ -0,0 +1,13 @@
+package EcommerceMain.EcomWeb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
